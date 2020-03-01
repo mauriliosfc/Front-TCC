abf5efe941983773cc1765b2d92a09d02b5ac43c
Maurílio B. Martins <maurilio.martins@tcsindustrial.com.br>
2020-03-01 18:09:27 -0300

    usando vue router

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>tesssssssssssssssssssste</h1>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style>
+</style>
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import Login from './components/Login'
+import Home from './components/Home'
+
+Vue.use(VueRouter)
+
+export default new Router({
+    routes: [
+        {
+            name: 'login',
+            path: '/login',
+            component: Login
+        },
+        {
+            name: 'home',
+            path: '/',
+            component: Home
+        },
+    ],
+})
