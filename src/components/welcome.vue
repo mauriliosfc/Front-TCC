3c46811537788896c7c4d7ac9a3dc5d585976642
juliocomendes <juliocomendes92@gmail.com>
2021-03-06 15:25:04 -0300

    alterações Flavio 3

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -18,7 +18,7 @@
         <img src="../assets/n.png" alt="NOIS" />
       </div>
       <div class="copyright">
-        Copyright© 2021  Todos os direitos reservados.
+        Copyright© 2021 Nois, Todos os direitos reservados.
       </div>
     </div>
   </div>
