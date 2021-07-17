68d97ccf9e40bc3e58e75f5611a8686be22c3861
juliocomendes <juliocomendes92@gmail.com>
2021-07-16 22:38:49 -0300

    alinhado a esquerda

--- src/components/Consult.vue
+++ src/components/Consult.vue
@@ -81,7 +81,7 @@
                     />
                   </v-col>
                 </v-row>
-                <v-row align="center" justify="center">
+                <v-row align="right" justify="right">
                   <v-col class="d-flex" cols="12" sm="6">
                     <v-text-field
                       :label="text.DC"
