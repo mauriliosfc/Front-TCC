01b1a784691c67e34b68597b614c049c665ec12a
juliocomendes <juliocomendes92@gmail.com>
2021-07-17 10:29:58 -0300

    atualizando timeline

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <div id="comboboxContatos">
+    <v-app id="fundo1">
+      <div id="comboboxContatos">
       <v-combobox
         @change="setLanguage()"
         v-model="language"
@@ -10,7 +11,6 @@
         v-on:change="onChange"
       ></v-combobox>
     </div>
-    <v-app>
       <v-timeline>
         <v-timeline-item v-for="n in 1" :key="n" large>
           <template v-slot:icon>
@@ -21,7 +21,7 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2 " color="amber lighten-1">
             <v-card-title class="headline">1º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1GFKH4ykNYnkr1NCMyklvKMmBm04wGQqw/view?usp=sharing">1º ICPIC</a></v-card-text
             >
@@ -36,7 +36,7 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber lighten-1">
             <v-card-title class="headline">2º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1V-LTReJUjKgfOnGXaas5TvVrp1YRbCN-/view?usp=sharing">2º ICPIC</a></v-card-text
             >
@@ -51,7 +51,7 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber lighten-1">
             <v-card-title class="headline">3º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1pD2RPDBwOPn3yydtXVGC1-47eu8vl_Rm/view?usp=sharing">3º ICPIC</a></v-card-text
             >
@@ -66,7 +66,7 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber lighten-1">
             <v-card-title class="headline">4º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/116pJkKqiwZmE3vbObboD9SF1LiNe2ypL/view?usp=sharing">4º ICPIC</a></v-card-text
             >
@@ -81,13 +81,13 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber lighten-1">
             <v-card-title class="headline">5º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1W2Izo_YYT8L5-1IwMN5bOs_2DWpB23qS/view?usp=sharing">5º ICPIC</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large >
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/icpic 2019.png"/>
@@ -96,13 +96,13 @@
           <template v-slot:opposite>
             <span>ICPIC - {{ text.suica }} 2019</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber lighten-1">
             <v-card-title class="headline">6º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1uQLQe5ypQryQrfyo0GJa9lyWovrfKJiH/view?usp=sharing">6º ICPIC</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/shea-cdc-.png"/>
@@ -111,13 +111,13 @@
           <template v-slot:opposite>
             <span>SHEA DECENNIAL 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-2">
             <v-card-title class="headline">7º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1__t-CkAyvdt5-fPkWLs7iq4ww2XomJdV/view?usp=sharing">1º SHEA dECENNIAL 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2"> 
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/shea-cdc-.png"/>
@@ -126,13 +126,13 @@
           <template v-slot:opposite>
             <span>SHEA DECENNIAL 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-2">
             <v-card-title class="headline">8º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1ttHYvX6qWvZp5RmECSWSQJLMaQp6zE8j/view?usp=sharing">2º SHEA dECENNIAL 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/shea-cdc-.png"/>
@@ -141,13 +141,13 @@
           <template v-slot:opposite>
             <span>SHEA DECENNIAL 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-2">
             <v-card-title class="headline">9º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1kmT5qGsZZbZ50Q1NuDC3MPzTdVIDy9Nh/view?usp=sharing">3º SHEA dECENNIAL 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/shea-cdc-.png"/>
@@ -156,13 +156,13 @@
           <template v-slot:opposite>
             <span>SHEA DECENNIAL 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-2">
             <v-card-title class="headline">10º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1btrTssba-zeZ73bcb5EP4gxm5hEvBfJ8/view?usp=sharing">4º SHEA dECENNIAL 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/shea-cdc-.png"/>
@@ -171,13 +171,13 @@
           <template v-slot:opposite>
             <span>SHEA DECENNIAL 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-2">
             <v-card-title class="headline">11º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1QkrM-IGAb8OPvFkTuhcmczZgEm7LH9zh/view?usp=sharing">5º SHEA dECENNIAL 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2" >
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -186,13 +186,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-4">
             <v-card-title class="headline">12º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/14Vu5KpSLP-P0tJ2pgSBliHgz28iuibLQ/view?usp=sharing">1º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -201,13 +201,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2"  color="amber darken-4">
             <v-card-title class="headline">13º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1canyQE9YQVhr0ghUkO9Exd3nD1svlg0f/view?usp=sharing">2º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -216,13 +216,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-4">
             <v-card-title class="headline">14º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1wkOfHhFBd5rl2GyZPbwXB0yp0-QpY_1N/view?usp=sharing">3º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -231,13 +231,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2"  color="amber darken-4">
             <v-card-title class="headline">15º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1wsYf7peztdNA8OMXduBf1zkygS-gaM-P/view?usp=sharing">4º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -246,13 +246,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2"  color="amber darken-4">
             <v-card-title class="headline">15º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1_m1Jdr8eHxl40aZuljUgo5RcUCsGtrAk/view?usp=sharing">5º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -261,13 +261,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2"  color="amber darken-4">
             <v-card-title class="headline">16º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1O464QrfU-Dk9KhxkdXt3mCrB0PP1Q5xB/view?usp=sharing">6º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -276,13 +276,13 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2" color="amber darken-4">
             <v-card-title class="headline">17º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/18oTAaIi6EsBUByzPS78xXlgDRu5fZykc/view?usp=sharing">7º IDWEEK 2020</a></v-card-text
             >
           </v-card>
         </v-timeline-item>
-        <v-timeline-item v-for="n in 1" :key="n" large>
+        <v-timeline-item v-for="n in 1" :key="n" large color="grey lighten-2">
           <template v-slot:icon>
             <v-avatar>
               <img src="../assets/IDWeek-Chasing-the-Sun_Full-Logo-OL-300x186.png"/>
@@ -291,7 +291,7 @@
           <template v-slot:opposite>
             <span>IDWEEK 2020</span>
           </template>
-          <v-card class="elevation-2">
+          <v-card class="elevation-2"  color="amber darken-4">
             <v-card-title class="headline">18º {{ text.artigos }}</v-card-title>
             <v-card-text><a href="https://drive.google.com/file/d/1iY9XOCYiybmGDGK1gHWKqQk8eB9Kzx4d/view?usp=sharing">8º IDWEEK 2020</a></v-card-text
             >
@@ -343,4 +343,8 @@ export default {
 </script>
 
 <style>
+#fundo1{
+  position: relative;
+  background-color:#fdf7e2;
+}
 </style>
