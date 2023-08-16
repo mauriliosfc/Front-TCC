82b94a3200feb9bcd9325026ce46da45e7d20520
juliocomendes <juliocomendes92@gmail.com>
2023-08-16 15:41:10 -0300

    ajuste login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,28 +3,22 @@
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
+          <v-col cols="12" sm="8" md="10">
             <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
+              <v-toolbar :style="toolbarStyle" dark flat>
                 <v-toolbar-title>Login</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-text-field label="Login" v-model="email" name="login" type="text" />
 
-                  <v-text-field
-                    id="password"
-                    v-model="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
+                  <v-text-field id="password" v-model="password" label="Password" name="password" type="password" />
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary" @click="login">Login</v-btn>
-                <v-btn color="primary" @click="register">Sign</v-btn>
+                <v-btn class="btn" @click="login" style="color: white;">Login</v-btn>
+                <v-btn class="btn text-white" @click="register" style="color: white;">Sign</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -43,6 +37,9 @@ export default {
     return {
       password: null,
       email: null,
+      toolbarStyle: {
+        background: 'linear-gradient(to right, #4a8df1, #61d3db)',
+      },
     };
   },
   methods: {
@@ -73,9 +70,14 @@ export default {
 </script>
 
 <style>
-#inspire{
+#inspire {
   position: relative;
   background-size: cover;
+  background-image: linear-gradient(to right, #4BC3CC, #2C7AED);
 }
 
+.btn {
+  background-image: linear-gradient(to right, #4BC3CC, #2C7AED);
+  color: white;
+}
 </style>
